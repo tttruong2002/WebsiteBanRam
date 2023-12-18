0457d74a8b615525eb45958b6db71553611cb157
RuthRePo <tttruong2002hcm@gmail.com>
2023-12-18 23:05:10 +0700

    Add new project WebsiteBanRam

---
+++ src/main/java/connect/DBConnect.java
@@ -0,0 +1,33 @@
+package connect;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnect {
+	private final String serverName = "localhost";
+	private final String dbName = "QuanLyBanRam";
+	private final String portNumber = "1433";
+	private final String instance = "";// LEAVE THIS ONE EMPTY IF YOUR SQL IS A SINGLE INSTANCE
+	private final String userID = "sa";
+	private final String password = "12345";
+
+	public Connection getConnection() throws Exception {
+		String url = "jdbc:sqlserver://" + serverName + "\\" + instance + ";" + "integratedSecurity=true;databaseName="
+				+ dbName;
+		if (instance == null || instance.trim().isEmpty())
+			url = "jdbc:sqlserver://" + serverName + ":" + portNumber + ";" + "integratedSecurity=true;databaseName="
+					+ dbName;
+		// DriverManager.registerDriver(new
+		// com.microsoft.sqlserver.jdbc.SQLServerDriver());
+		Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+		return DriverManager.getConnection(url);
+	}
+
+	public static void main(String[] args) {
+		try {
+			System.out.println(new DBConnect().getConnection());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
